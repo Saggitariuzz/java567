db613df80b34940a10c36d34e8c2d40b71295cb9
Andrey <andreykoccc@gmail.com>
2024-10-22 12:25:25 +0300

    базу подвязал))

---
+++ backend/cityadministration/src/main/java/com/cityadministration/CityadministrationApplication.java
@@ -0,0 +1,13 @@
+package com.cityadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityadministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityadministrationApplication.class, args);
+	}
+
+}
